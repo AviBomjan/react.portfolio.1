b4a7932b6caac824909a2b256cbf377677e6aa46
yourusername <avibomjan@gmail.com>
2022-03-22 00:25:00 +0800

    Ver 1.0
    First Version

---
+++ src/components/about/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+        <div className="about__me-image">
+          <img src={ME} alt="About Image" />
+        </div>
+        </div>
+        <div className='about__content'>
+          <div className="about__cards">
+
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1.5+ Years Working</small>
+            </article>
+            
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Worked in</h5>
+              <small>2 companies</small>
+            </article>
+            
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5+ Completed</small>
+            </article>
+
+          </div>
+          <p>
+          I am a web developer with 1.5 years of work experience and a passion for a career in web and application development. I come to the industry with my skills in JavaScript (React), SQL, Python, Java, PHP, Swift and wish to learn how to utilize these alongside my soft skills, so that I can a career path.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
